41ba3ae413fd4eb7231c386819ca98b87745e74d
Tyler Silverman <silverman.tyler@gmail.com>
2021-02-04 15:06:56 -0500

    added npm build folders and also models

---
+++ build/models/transaction.js
@@ -0,0 +1,34 @@
+// var mongoose = require("mongoose");
+// var Schema = mongoose.Schema;
+
+// var transactionSchema = new Schema(
+//     {
+//       firstName: {
+//         type: String,
+//         trim: true,
+//         required: "Enter a name "
+//       },
+//       lastName: {
+//           type: String,
+//           trim: true,
+//           required: "Enter a name"
+//       },
+//         emailName: {
+//           type: String,
+//           trim: true,
+//       },
+//         phoneNumber: {
+//         type: Number,
+//         required: "Enter an number"
+//       },
+//       date: {
+//         type: Date,
+//         default: Date.now
+//       }
+//     }
+//   );
+  
+
+// var Transaction = mongoose.model("Transaction", transactionSchema);
+
+// module.exports = Transaction;
