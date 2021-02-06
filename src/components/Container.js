d2d698c94c52263d8d49c1eb25c748b410b739b7
Tyler Silverman <silverman.tyler@gmail.com>
2021-02-06 11:31:28 -0500

    added table

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,8 @@ import React from "react";
 
 function Container(props) {
   console.log("container")
-  return <div className={`container${props.fluid ? "-fluid" : ""}`}>{props.children}</div>;
+  return <div className={`container${props.fluid ? "-fluid" : ""}`}>{props.children}
+  </div>;
   
 }
 
--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -2,77 +2,71 @@ import React from "react";
 import API from "../utils/API";
 
 class UserDetail extends React.Component {
-    constructor (props){
-        super(props);
-        this.state ={
-            users: null,
-            first: "",
-            last: "",
-            gender: "",
-            email:"",
-            phone:"",
-            dob: ""
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: null,
+      first: "",
+      last: "",
+      gender: "",
+      email: "",
+      phone: "",
+      dob: ""
     }
+  }
 
-    
-    componentDidMount (){
-        API.getUsers().then(data => {
-            console.log(data + "componentDidMount function")
-            console.log(data.data.results)
-            this.setState({
-                users: data.data.results
-            })
-        })
-    }
 
-    render() {
-        const users = this.state.users;
+  componentDidMount() {
+    API.getUsers().then(data => {
+      console.log(data + "componentDidMount function")
+      console.log(data.data.results)
+      this.setState({
+        users: data.data.results
+      })
+    })
+  }
 
-        return (
-            <div className="text">
-                {users && (users.map(user => {
+  render() {
+    const users = this.state.users;
 
-                   return (
-                    <table class="table">
-                    <thead class="thread-dark">
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name:</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Gender</th>
-                        <th scope="col">Email Address</th>
-                        <th scope="col">Phone Number</th>
-                        <th scope="col">Years Old</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <th scope="col">{user.name.first}</th>
-                        <td>{user.name.last}</td>
-                        <td>{user.gender}</td>
-                        <td>{user.email}</td>
-                        <td>{user.phone}</td>
-                        <td>{user.dob.age}</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">2</th>
-                        <th scope="col">{user.name.first}</th>
-                        <td>{user.name.last}</td>
-                        <td>{user.gender}</td>
-                        <td>{user.email}</td>
-                        <td>{user.phone}</td>
-                        <td>{user.dob.age}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  );
-                    
-                }))}
-            </div> 
-        )
-    }
+    return (
+      
+      <div className="table thread">
+           <table class="table">
+              <thead class="thread">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">First Name:</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Gender</th>
+                  <th scope="col">Email Address</th>
+                  <th scope="col">Phone Number</th>
+                  <th scope="col">Years Old</th>
+                </tr>
+              </thead>
+            </table>
+        {users && (users.map(user => {
+          return (
+            <table class="table">
+              <tbody>
+                <tr>
+                  <th scope="row">#</th>
+                  <td>{user.name.first}</td>
+                  <td>{user.name.last}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.dob.age}</td>
+                </tr>
+              </tbody>
+            </table>
+
+          );
+
+        }))}
+      </div>
+    )
+  }
 
 }
 export default UserDetail;
